f3654302e9c58f87e7033ebb21a031f34faecf8f
Steve Dongich <stevowny@gmail.com>
2025-04-21 16:22:30 +0300

    debug index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/api/transactions', transactionRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(process.env.PORT, () =>
