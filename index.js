fb79c8e697d52b42b5331f69c34b74d73497809b
Steve Dongich <stevowny@gmail.com>
2025-04-27 22:22:26 +0700

    set up PORT variable

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 3001
 
 // Middleware
 app.use(cors());
@@ -22,7 +23,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(process.env.PORT, () =>
+    app.listen(PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
   })
