ec21b8856f6e4f4b61d1da3e4977b0c22b8e9e44
Steve Dongich <stevowny@gmail.com>
2025-04-28 05:17:42 +0700

    debug for cron

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const job = require('./lib/cron')
+// const job = require('./lib/cron')
+import job from "./lib/cron"
 const app = express();
 const PORT = process.env.PORT || 3001
 
