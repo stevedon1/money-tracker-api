3e6c08cec09605a4684a74f6d6cb60811948f3a9
Steve Dongich <stevowny@gmail.com>
2025-05-16 16:14:58 +0300

    adjust category enum in transaction model

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,8 +3,23 @@ import mongoose from 'mongoose';
 const transactionSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   type: { type: String, enum: ['income', 'expense'], required: true },
-  category: { type: String, required: true },
-  paymentMethod: { type: String, enum: ['mobile money', 'cash'], required: true },
+  category: {
+  type: String,
+  enum: [
+    'Housing',
+    'Food & Groceries',
+    'Transportation',
+    'Income & Salary',
+    'Utilities & Bills',
+    'Savings & Investments',
+    'Health & Insurance',
+    'Education & Childcare',
+    'Entertainment & Lifestyle',
+    'Miscellaneous'
+  ],
+  required: true
+},
+  paymentMethod: { type: String, enum: ['mpesa', 'cash', 'bank'], required: true },
   description: String,
   amount: { type: Number, required: true },
   date: { type: Date, default: Date.now }
